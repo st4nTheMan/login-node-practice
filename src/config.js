b490a9b6457abf9d68b007b57affbe1821057420
st4nTheMan <stanleysoco@gmail.com>
2025-03-17 16:06:04 +0800

    Modified homepage and added mysql2 package

--- src/config.js
+++ src/config.js
@@ -12,6 +12,10 @@ const loginSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    email: {
+        type: String,
+        required: true
+    },
     password: {
         type: String,
         required: true
